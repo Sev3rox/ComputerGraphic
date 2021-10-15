1099e4422158821e5695b7d2169ae110fa6c433e
Maciek <mjstyly@gmail.com>
2021-10-15 22:24:08 +0200

    2nd

--- Project2/Project2/MainWindow.xaml.cs
+++ Project2/Project2/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System;
 using System.IO;
 using System.Drawing;
 using Microsoft.Win32;
+using System.Drawing.Imaging;
 
 namespace Project2
 {
@@ -24,6 +25,9 @@ namespace Project2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Bitmap bitmap;
+        BitmapImage bitmap2;
+        int save = 1;
         public MainWindow()
         {
             InitializeComponent();
@@ -63,7 +67,6 @@ namespace Project2
             int height = 0;
             int max = 0;
             int line = 1;
-            Bitmap bitmap;
             bool p6 = true;
             char ppmtype;
             var reader = new BinaryReader(new FileStream(file, FileMode.Open), Encoding.ASCII);
@@ -135,13 +138,15 @@ namespace Project2
             bitmap = new Bitmap(width, height);
             int x = 0, y = 0;
 
+            double maxdouble = ((double)max / (double)255);
+
             if (p6)
             {
                 while (reader.BaseStream.Position != reader.BaseStream.Length)
                 {
 
                     if (reader.BaseStream.Position + 2 < reader.BaseStream.Length)
-                        bitmap.SetPixel(x, y, System.Drawing.Color.FromArgb(reader.ReadByte(), reader.ReadByte(), reader.ReadByte()));
+                        bitmap.SetPixel(x, y, System.Drawing.Color.FromArgb((int)(reader.ReadByte() / maxdouble), (int)(reader.ReadByte() / maxdouble), (int)(reader.ReadByte() / maxdouble)));
                     x++;
                     if (x >= width)
                     {
@@ -154,7 +159,6 @@ namespace Project2
             }
             else
             {
-                max = max / 255;
                 int x1 = -1;
                 int x2 = -1;
                 int x3 = -1;
@@ -192,7 +196,7 @@ namespace Project2
                         {
                             x3 = Int32.Parse(xstr);
 
-                            bitmap.SetPixel(x, y, System.Drawing.Color.FromArgb(x1 / max, x2 / max, x3 / max));
+                            bitmap.SetPixel(x, y, System.Drawing.Color.FromArgb((int)(x1 / maxdouble), (int)(x2 / maxdouble), (int)(x3 / maxdouble)));
                             x++;
                             if (x >= width)
                             {
@@ -214,13 +218,137 @@ namespace Project2
             this.BitmapToImageSource(bitmap);
         }
 
+        public void ReadJpeg(string file)
+        {
+            bitmap2 = new BitmapImage();
+            bitmap2.BeginInit();
+            bitmap2.UriSource = new Uri(file);
+            bitmap2.EndInit();
+            img.Source=bitmap2;
+            this.img.Width = bitmap2.Width;
+            if (bitmap2.Width > 1400)
+                this.img.Width = 1400;
+            if (bitmap2.Width < 100)
+                this.img.Width = 100;
+            this.img.Height = bitmap2.Height;
+            if (bitmap2.Height > 900)
+                this.img.Height = 900;
+            if (bitmap2.Height < 100)
+                this.img.Height = 100;
+        }
+
         private void chooseFile(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new();
 
             if (fileDialog.ShowDialog() == true)
             {
-                ReadBitmapFromPPM(fileDialog.FileName);
+                string ext = System.IO.Path.GetExtension(fileDialog.FileName);
+                if (ext == ".ppm")
+                {
+                    try
+                    {
+                        ReadBitmapFromPPM(fileDialog.FileName);
+                    }
+                    catch (InvalidCastException er)
+                    {
+                        MessageBox.Show("                           Error in file detected                     ");
+                    }
+                }
+                else if (ext == ".jpg" || ext == ".jpeg")
+                {
+                    bitmap = null;
+                    ReadJpeg(fileDialog.FileName);
+                }
+                else
+                {
+                    MessageBox.Show("                           Not correct file format                     ");
+                }
+            }
+        }
+
+private void saveFile(object sender, RoutedEventArgs e)
+        {
+            if (bitmap != null)
+            {
+                System.Drawing.Imaging.Encoder myEncoder;
+                EncoderParameter myEncoderParameter;
+                EncoderParameters myEncoderParameters;
+
+                myEncoder = System.Drawing.Imaging.Encoder.Quality;
+                myEncoderParameters = new EncoderParameters(1);
+
+                ComboBoxItem cbi = (ComboBoxItem)combo.SelectedItem;
+                int x = Int32.Parse(cbi.Content.ToString());
+
+                switch (x)
+                {
+                    case 10: myEncoderParameter = new EncoderParameter(myEncoder, 10L); break;
+
+                    case 25: myEncoderParameter = new EncoderParameter(myEncoder, 25L); break;
+
+                    case 50: myEncoderParameter = new EncoderParameter(myEncoder, 50L); break;
+
+                    case 75: myEncoderParameter = new EncoderParameter(myEncoder, 75L); break;
+
+                    default: myEncoderParameter = new EncoderParameter(myEncoder, 100L); break;
+                }
+
+                myEncoderParameters.Param[0] = myEncoderParameter;
+
+                ImageCodecInfo[] myCodecs = ImageCodecInfo.GetImageEncoders();
+                ImageCodecInfo jpegCodec = myCodecs[1];
+
+                bitmap.Save("../../../../saved.jpeg", jpegCodec, myEncoderParameters);
+            }
+            else if(bitmap2!=null)
+            {
+                System.Drawing.Imaging.Encoder myEncoder;
+                EncoderParameter myEncoderParameter;
+                EncoderParameters myEncoderParameters;
+
+                myEncoder = System.Drawing.Imaging.Encoder.Quality;
+                myEncoderParameters = new EncoderParameters(1);
+
+                ComboBoxItem cbi = (ComboBoxItem)combo.SelectedItem;
+                int x = Int32.Parse(cbi.Content.ToString());
+
+                switch (x)
+                {
+                    case 10: myEncoderParameter = new EncoderParameter(myEncoder, 10L); break;
+
+                    case 25: myEncoderParameter = new EncoderParameter(myEncoder, 25L); break;
+
+                    case 50: myEncoderParameter = new EncoderParameter(myEncoder, 50L); break;
+
+                    case 75: myEncoderParameter = new EncoderParameter(myEncoder, 75L); break;
+
+                    default: myEncoderParameter = new EncoderParameter(myEncoder, 100L); break;
+                }
+
+                myEncoderParameters.Param[0] = myEncoderParameter;
+
+                ImageCodecInfo[] myCodecs = ImageCodecInfo.GetImageEncoders();
+                ImageCodecInfo jpegCodec = myCodecs[1];
+
+                var help = BitmapImage2Bitmap(bitmap2);
+
+                help.Save("../../../../saved"+save+".jpeg", jpegCodec, myEncoderParameters);
+                save++;
+
+            }
+        }
+
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
             }
         }
     }
